c49d9dbc1070322803b4bab2e1e05d8c82349ccb
MDS <ericdavidsmith91@gmail.com>
2023-06-01 16:16:57 -0400

    Update BlueprintModifier.h

--- Source/Public/BlueprintModifier.h
+++ Source/Public/BlueprintModifier.h
@@ -1,1 +1,20 @@
+#pragma once
 
+#include "CoreMinimal.h"
+#include "BlueprintModifier.generated.h"
+
+UCLASS()
+class CHATGPT_UE_PLUGIN_API UBlueprintModifier : public UObject
+{
+    GENERATED_BODY()
+
+public:
+    // Function for receiving a Blueprint from the Unreal Engine plugin
+    void ReceiveBlueprint(FString Blueprint);
+
+    // Function for sending a modified Blueprint back to the Unreal Engine plugin
+    FString SendModifiedBlueprint();
+
+    // Functions for performing various modifications on a Blueprint
+    void ModifyBlueprint(FString Modification);
+};
