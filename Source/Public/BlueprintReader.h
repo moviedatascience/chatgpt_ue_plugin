4b7bfffbe812e96f341f818a41ef3b519018f981
MDS <ericdavidsmith91@gmail.com>
2023-06-01 16:12:11 -0400

    Update BlueprintReader.h

--- Source/Public/BlueprintReader.h
+++ Source/Public/BlueprintReader.h
@@ -1,1 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HttpModule.h"
+
+class FBlueprintReader : public IModuleInterface
+{
+public:
+    virtual void StartupModule() override;
+    virtual void ShutdownModule() override;
+    void ReadBlueprint();
+
+private:
+    void RegisterMenus();
+    void RegisterSettings();
+    void UnregisterSettings();
+    bool HandleSettingsSaved();
+    TSharedRef<class SDockTab> OnSpawnPluginTab(const class FSpawnTabArgs& SpawnTabArgs);
+    TSharedPtr<class FUICommandList> PluginCommands;
+};
 
